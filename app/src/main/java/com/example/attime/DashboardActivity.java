8140c390183b11fce4c654bc6c54f60bb28ea741
martin-ngigi <martinwainaina001@gmail.com>
2023-01-22 18:22:06 +0300

    Added Intents

--- app/src/main/java/com/example/attime/DashboardActivity.java
+++ app/src/main/java/com/example/attime/DashboardActivity.java
@@ -2,13 +2,29 @@ package com.example.attime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.attime.databinding.ActivityDashboardBinding;
+
 public class DashboardActivity extends AppCompatActivity {
+
+    ActivityDashboardBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        //setContentView(R.layout.activity_dashboard);
+
+        binding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.generateQRCV.setOnClickListener( e -> {
+            startActivity(new Intent(DashboardActivity.this, GenerateQRActivity.class));
+        });
+
+        binding.scanQRCV.setOnClickListener( e -> {
+            startActivity(new Intent(DashboardActivity.this, ScannedQRorBarcodeActivity.class));
+        });
     }
 }
